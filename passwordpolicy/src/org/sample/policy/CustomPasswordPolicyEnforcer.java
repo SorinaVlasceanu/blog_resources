5a473edf3ef4047d5c8c499b3f5caf0b2d50873d
ajith <ajithn@wso2.com>
2015-01-13 22:13:34 -0500

    adding new blog resource

---
+++ passwordpolicy/src/org/sample/policy/CustomPasswordPolicyEnforcer.java
@@ -0,0 +1,58 @@
+package org.sample.policy;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.identity.mgt.policy.AbstractPasswordPolicyEnforcer;
+import org.wso2.carbon.identity.mgt.policy.password.DefaultPasswordLengthPolicy;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by ajith on 1/13/15.
+ */
+public class CustomPasswordPolicyEnforcer extends AbstractPasswordPolicyEnforcer {
+
+    private static final Log log = LogFactory.getLog(DefaultPasswordLengthPolicy.class);
+
+    private int MIN_LENGTH;
+    private int MAX_LENGTH;
+    private String policyPattern;
+
+    @Override
+    public boolean enforce(Object... args) {
+        //check the policyPattern at the user adding as well, same done at the server initialization as well.
+        if (policyPattern == null) {
+            log.error("Failed to initialize the PasswordPolicy handler because policy pattern is null ");
+            return false;
+        }
+        //can't add empty password
+        if (args != null || args.length > 0) {
+            log.info("Password can't be empty");
+            return false;
+        }
+        //match the user password with the password policy.
+        String password = args[0].toString();
+        Pattern pattern = Pattern.compile(policyPattern);
+        Matcher matcher = pattern.matcher(password);
+        boolean result = matcher.matches();
+
+        if (!result) {
+            log.warn("Password should have one upper case, one lowercase and one digit and special character");
+        }
+        return result;
+    }
+
+    @Override
+    public void init(Map<String, String> properties) {
+        if (properties != null && properties.size() > 0) {
+            policyPattern = properties.get("pattern");
+            //Check at the server startup
+            if (policyPattern == null) {
+                log.error("Failed to initialize the PasswordPolicy handler because policy pattern is null ");
+            }
+        }
+    }
+}
+
